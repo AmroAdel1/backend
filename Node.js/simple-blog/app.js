a2c6121bb454977c20a148f5e6dc8641c1c0c391
AmroAdel1 <119890404+AmroAdel1@users.noreply.github.com>
2025-11-01 11:34:29 +0200

    Update app.js

--- Node.js/simple-blog/app.js
+++ Node.js/simple-blog/app.js
@@ -7,10 +7,10 @@ const blogRoutes = require('./routes/blogRoutes');
 const app = express();    // instance
 
 // connect to mongodb
-const dbURI = 'mongodb+srv://adel:amroadel123.@cluster0.dmzt7aq.mongodb.net/?appName=Cluster0'
+const dbURI = ''    // connect link
 
 mongoose.connect(dbURI)    // async
-  .then(result => app.listen(3000))    // success    
+  .then(result => app.listen(yourPortnumber))    // success     // port number
   .catch(err => console.log(err));
 
 // register view engine
@@ -41,100 +41,3 @@ app.use('/blogs', blogRoutes);    // activate router when go to /blogs
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
-
-/*
-const Blog = require('./models/blog');
-
-// app.set('views', 'myviews');       // change default views folder
-
-// listen for requests
-app.listen(3000);
-
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  next();     // need to stop middleware
-});
-
-app.use((req, res, next) => {
-  console.log('in the next middleware');
-  next();
-});
-
-app.get('/', (req, res) => {
-  // res.send('<p>home page</p>');
-  // res.sendFile('./views/index.html', { root: __dirname });    // like res.write and res.end, no need header   // default absolute path
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render('index', { title: 'Home', blogs });      // sending data to view    // blogs: blogs
-});
-
-app.get('/about', (req, res) => {
-  // res.send('<p>about page</p>');
-  //res.sendFile('./views/about.html', { root: __dirname });
-  res.render('about', { title: 'About' });
-});
-
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
-
-// 404 page
-app.use((req, res) => {       // middleware   // order matters  // can type any url
-  //res.status(404).sendFile('./views/404.html', { root: __dirname });
-  res.status(404).render('404', { title: '404' });
-});
-*/
-
-/*
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
-});
-*/
-
-/*
-// mongoose & mongo tests
-app.get('/add-blog', (req, res) => {      // go to localhost:3000/add-blog to add query in db
-  const blog = new Blog({
-    title: 'new blog',
-    snippet: 'about my new blog',
-    body: 'more about my new blog'
-  })
-
-  blog.save()       // save to db       // async   // takes time
-    .then(result => {
-      res.send(result);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.get('/all-blogs', (req, res) => {
-  Blog.find()
-    .then(result => {
-      res.send(result);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.get('/single-blog', (req, res) => {
-  Blog.findById('6904fa1a84a6e0161b83870f')
-    .then(result => {
-      res.send(result);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-*/
-
-// filter blogs by category, add author
