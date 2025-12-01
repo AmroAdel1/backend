856a99f1dff2b583894cd4cc7629be7d34f64a4a
AmroAdel1 <119890404+AmroAdel1@users.noreply.github.com>
2025-12-01 02:49:16 +0200

    Update app.blade.php

--- php/simple-blog-posts/resources/views/layouts/app.blade.php
+++ php/simple-blog-posts/resources/views/layouts/app.blade.php
@@ -337,7 +337,7 @@
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{ route('posts.index') }}">
-                📝 Blogs
+                BlogsHub
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
