7bea7a488d41bda80c2a4655d18d6a676b5c5838
AmroAdel1 <119890404+AmroAdel1@users.noreply.github.com>
2025-12-31 21:19:27 +0200

    Update web.php

--- php/simple-todolist/routes/web.php
+++ php/simple-todolist/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ForgotPasswordController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ContactController;
 
 // Guest routes (not authenticated)
 Route::middleware('guest')->group(function () {
@@ -23,9 +25,30 @@ Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
     Route::get('/settings', [AuthController::class, 'settings'])->name('auth.settings');
 
+    // Profile routes
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password');
+    Route::post('/profile/avatar', [ProfileController::class, 'updateAvatar'])->name('profile.avatar');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Static Pages Routes
+    Route::get('/about-us', function () {
+        return view('about');
+    })->name('about');
+    Route::get('/privacy-policy', function () {
+        return view('privacy-policy');
+    })->name('privacy-policy');
+    Route::get('/terms-of-service', function () {
+        return view('terms-of-service');
+    })->name('terms-of-service');
+
+    // Contact Us Route
+    Route::get('/contact-us', [ContactController::class, 'index'])->name('contact.index');
+    Route::post('/contact-us', [ContactController::class, 'submit'])->name('contact.submit');
+
     // Todo routes
     Route::get('/todos/finished', [TodoController::class, 'finished'])->name('todos.finished');
     Route::resource('todos', TodoController::class);
     Route::patch('/todos/{todo}/toggle', [TodoController::class, 'toggleComplete'])->name('todos.toggle');
 });
-
