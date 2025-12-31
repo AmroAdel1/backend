0d07f3f2f3c9db4dd135a0ee9a03827496e9aa82
AmroAdel1 <119890404+AmroAdel1@users.noreply.github.com>
2025-12-31 22:00:08 +0200

    Update contact-auto-reply.blade.php

--- php/simple-todolist/resources/views/mail/contact-auto-reply.blade.php
+++ php/simple-todolist/resources/views/mail/contact-auto-reply.blade.php
@@ -4,14 +4,10 @@
     <title>We Received Your Message</title>
 </head>
 <body>
-    <h2>Thank You for Contacting Us</h2>        <br>
-
-    <p>We've received your message and will get back to you as soon as possible.</p>    <br>
-
-    <p>Our team typically responds within 24-48 hours.</p>      <br>
-
-    <p>For urgent matters, please call us at [Your Phone Number].</p>       <br>
-
+    <h2>Thank You for Contacting Us</h2>
+    <p>We've received your message and will get back to you as soon as possible.</p>
+    <p>Our team typically responds within 24-48 hours.</p>
+    <p>For urgent matters, please call us at [Your Phone Number].</p>
     <p>Best regards,<br>
     The {{ config('app.name') }} Team</p>
 </body>
