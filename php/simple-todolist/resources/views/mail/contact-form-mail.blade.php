3537ec70cbd44c8b7e933c8c8fa612a31e1d2663
AmroAdel1 <119890404+AmroAdel1@users.noreply.github.com>
2025-12-31 22:04:33 +0200

    Update contact-form-mail.blade.php

--- php/simple-todolist/resources/views/mail/contact-form-mail.blade.php
+++ php/simple-todolist/resources/views/mail/contact-form-mail.blade.php
@@ -4,12 +4,12 @@
     <title>New Contact Form Submission</title>
 </head>
 <body>
-    <h2>New Contact Form Submission</h2>                        <br>
+    <h2>New Contact Form Submission</h2>                        
 
-    <p><strong>Name: </strong> {{ $data['name'] }}</p>          <br>
-    <p><strong>Email: </strong> {{ $data['email'] }}</p>        <br>
-    <p><strong>Message: </strong></p>                           <br>
-    <p>{{ $data['message'] }}</p>                               <br>
+    <p><strong>Name: </strong> {{ $data['name'] }}</p>          
+    <p><strong>Email: </strong> {{ $data['email'] }}</p>       
+    <p><strong>Message: </strong></p>                           
+    <p>{{ $data['message'] }}</p>                               
 
     <p>Received at: {{ now()->format('Y-m-d H:i:s') }}</p>
 </body>
